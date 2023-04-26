6b20e18f7d74ce51089376d706741fe66d9d8410
Boran Kaya <borankaya210@gmail.com>
2023-04-26 23:29:41 +0300

    Cryp Şifreleme Eklendi

--- ProjectDAL/Init/MyInit.cs
+++ ProjectDAL/Init/MyInit.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Project.COMMON.Tools;
+using ProjectDAL.Context;
+using ProjectENTİTİES.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectDAL.Init
 {
-    public class MyInit
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
     {
+        protected override void Seed(MyContext context)
+        {
+            #region Admin
+
+            AppUser au = new AppUser();
+            au.UserName = "cgr";
+            au.Password = DantexCrypt.Crypt("123");
+
+
+
+            #endregion
+        }
     }
 }
