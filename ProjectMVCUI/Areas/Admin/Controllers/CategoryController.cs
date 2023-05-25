b67ef78054820d0efb3539fa8217f3cb928364da
Boran Kaya <borankaya210@gmail.com>
2023-05-26 02:46:29 +0300

    Product Admin Paneli yapıldı...Ancak Product Ekleme,Silme ve Güncelleme daha devam edecek

--- ProjectMVCUI/Areas/Admin/Controllers/CategoryController.cs
+++ ProjectMVCUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ProjectBLL.Repositories.ConcRep;
+using ProjectENTİTİES.Models;
+using ProjectMVCUI.Areas.Admin.Data.AdminPageVMs;
+using ProjectVM.PureVMs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,82 @@ namespace ProjectMVCUI.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: Admin/Category
-        public ActionResult ListCategories()
+        CategoryRepository _cRep;
+
+        public CategoryController()
         {
+            _cRep = new CategoryRepository();
+        }
+        public ActionResult ListCategories(int? id)
+        {
+
+            List<AdminCategoryVM> categories;
+            if (id == null)
+            {
+                categories = _cRep.Select(x => new AdminCategoryVM
+                {
+                    ID = x.ID,
+                    CategoryName = x.CategoryName,
+                    DeletedDate = x.DeletedDate,
+                    CreatedDate = x.CreatedDate,
+                    Description = x.Description,
+                    ModifiedDate = x.ModifiedDate,
+                    Status = x.Status.ToString(),
+                }).ToList();
+            }
+            else
+            {
+               categories = _cRep.Where(x => x.ID == id).Select(x => new AdminCategoryVM
+                {
+                    ID=x.ID,
+                    CategoryName = x.CategoryName,
+                    DeletedDate = x.DeletedDate,
+                    CreatedDate = x.CreatedDate,
+                    Description = x.Description,
+                    ModifiedDate = x.ModifiedDate,
+                    Status = x.Status.ToString(),
+                }).ToList() ;
+            }
+
+            AdminCategoryListPageVM alisVm = new AdminCategoryListPageVM
+            {
+                Categories = categories
+            };
+
+
+
+            return View(alisVm);
+        }
+
+
+        public ActionResult AddCategory()
+        { 
             return View();
         }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            _cRep.Add(category);
+            return RedirectToAction("ListCategory");
+        }
+
+        public ActionResult UpdateCategory(int id)
+        {
+            return View(_cRep.Find(id)); 
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category) 
+        {
+        _cRep.Update(category);
+            return RedirectToAction("ListCategories");
+       }
+
+        public ActionResult DeleteCategory(int id) 
+        {
+            _cRep.Delete(_cRep.Find(id));
+            return RedirectToAction("ListCategories");
+
+        }
+        
     }
-}
+} 
---
+++ ProjectMVCUI/Areas/Admin/Data/AdminPageVMs/AdminCategoryListPageVM.cs
@@ -0,0 +1,13 @@
+﻿using ProjectVM.PureVMs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectMVCUI.Areas.Admin.Data.AdminPageVMs
+{
+    public class AdminCategoryListPageVM
+    {
+        public List<AdminCategoryVM> Categories { get; set; }
+    }
+}
